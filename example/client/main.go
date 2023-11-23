f6ca85f52ef9d5d154fe83be99bd0b9fb12ff27f
violapioggia <2604296771@qq.com>
2023-11-23 22:33:57 +0800

    docs:optimize readme

--- example/client/main.go
+++ example/client/main.go
@@ -38,8 +38,6 @@ func (cl *configLog) Apply(opt *utils.Options) {
 }
 
 func main() {
-	klog.SetLevel(klog.LevelDebug)
-
 	etcdClient, err := etcd.NewClient(etcd.Options{})
 	if err != nil {
 		panic(err)
@@ -47,8 +45,8 @@ func main() {
 
 	cl := &configLog{}
 
-	serviceName := "ServiceName"
-	clientName := "ClientName"
+	serviceName := "ServiceName" // your server-side service name
+	clientName := "ClientName"   // your client-side service name
 	client, err := echo.NewClient(
 		serviceName,
 		client.WithHostPorts("0.0.0.0:8888"),
